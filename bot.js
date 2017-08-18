009fea1c3ecf01916b0ed81c7b77d712937c59e6
BuildTools <Spacebug300@users.noreply.github.com>
2017-08-18 11:29:31 +0300

    Wrote initialization code
    Since getting the bot online is quite important believe it or not

---
+++ bot.js
@@ -0,0 +1,12 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+//Configure Logger
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {colorize: true});
+logger.level = 'debug';
+
+//Initialize Bot
+var bot = new Discord.Client({token: auth.token, autorun: true});
+bot.on('ready', function (evt) {logger.info('Connected'); logger.info('Logged in as: '); logger.info(bot.username + ' - (' + bot.id + ')');});
