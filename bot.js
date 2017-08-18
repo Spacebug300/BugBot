94e7664b182b92ff3d9a0e4059ccb2e1ba66b773
BuildTools <Spacebug300@users.noreply.github.com>
2017-08-18 15:40:33 +0300

    Added commands

--- bot.js
+++ bot.js
@@ -10,3 +10,22 @@ logger.level = 'debug';
 //Initialize Bot
 var bot = new Discord.Client({token: auth.token, autorun: true});
 bot.on('ready', function (evt) {logger.info('Connected'); logger.info('Logged in as: '); logger.info(bot.username + ' - (' + bot.id + ')');});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // BugBot needs to know when to listen for commands
+    // In this case, tt will listen for messages that will start with `b;`
+    if (message.substring(0, 1) == 'b;') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            case 'bugbot':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hello!'
+                });
+            break;
+         }
+     }
+});
